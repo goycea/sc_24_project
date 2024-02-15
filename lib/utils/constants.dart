eddd119a330a9e22a50e111e5d264f904154578e
Alperen Göyce <goycea@gmail.com>
2024-02-15 17:07:05 +0300

    map updates

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,5 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import '../models/coordinate_model.dart';
 
 final List<CoordinateModel> coordinateList = [
@@ -75,3 +77,17 @@ CoordinateModel("Çankaya", "Barış Parkı", [39.8881761878092, 32.819414148193
 CoordinateModel("Çankaya", "Botanik Parkı", [39.887941857575, 32.8565299870627]),
 CoordinateModel("Çankaya", "Sultan Fatih Parkı", [39.8831119008777, 32.6840806066307]),
 CoordinateModel("Çankaya", "Balgat Aliye Yahşi Meslek Lisesi Bah.", [39.9085965929133, 32.8159337537918]),];
+
+final markers= [Marker(
+markerId: MarkerId("Yenilik İO Bahçesi"),
+position: LatLng(39.864217229349, 32.8303028059098),
+infoWindow: InfoWindow(title: "Yenilik İO Bahçesi"),
+),Marker(
+  markerId: MarkerId("Balgat Aliye Yahşi Meslek Lisesi Bah."),
+  position: LatLng(39.9085965929133, 32.8159337537918),
+  infoWindow: InfoWindow(title: "Balgat Aliye Yahşi Meslek Lisesi Bah."),
+),Marker(
+  markerId: MarkerId("Kuğulu Park"),
+  position: LatLng(39.9019095674532, 32.8601492660582),
+  infoWindow: InfoWindow(title: "Kuğulu Park"),
+),];
--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -4,6 +4,8 @@ import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:ui' as ui;
 
+import '../utils/constants.dart';
+
 class MapView extends StatefulWidget {
   const MapView({super.key});
 
@@ -21,37 +23,18 @@ class _MapViewState extends State<MapView> {
   );
 
   static const CameraPosition _kLake = CameraPosition(
-      target: LatLng(45.44266391721894, 10.99873082349294),
+      target: LatLng(39.9019095674532, 32.8601492660582),
       zoom: 15.151926040649414);
-
+  
+  Set<Marker> markers = {}; 
+  
   @override
-  void initState() {
+  void initState() { 
     super.initState();
-    //_getCustomMarker();
+    markers = coordinateList.map((e) {return Marker(markerId:MarkerId("${e.name}"),infoWindow: InfoWindow(title: "${e.name}") ,
+        position: LatLng(e.coordinates.first,e.coordinates.last));}).toSet();
   }
 
-  static const _julietHome = LatLng(45.44266391721894, 10.99873082349294);
-  static const _romeoHome = LatLng(45.444300459532826, 10.999193135733986);
-  static const _veronaArena = LatLng(45.43964270398411, 10.994390997076065);
-  static const _verona = LatLng(45.43824320692251, 10.991844909323326);
-
-
-
-  BitmapDescriptor romeoIcon = BitmapDescriptor.defaultMarker;
-  BitmapDescriptor julietIcon = BitmapDescriptor.defaultMarker;
-  BitmapDescriptor arenaIcon = BitmapDescriptor.defaultMarker;
-  BitmapDescriptor veronaIcon = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRose);
-
-
-  Marker createMarker(
-      String id, LatLng position, BitmapDescriptor icon, String title) {
-    return Marker(
-      markerId: MarkerId(id),
-      position: position,
-      icon: icon,
-      infoWindow: InfoWindow(title: title),
-    );
-  }
 
   Future<Uint8List> getBytesFromAsset(String path, int width) async {
     ByteData data = await rootBundle.load(path);
@@ -63,11 +46,13 @@ class _MapViewState extends State<MapView> {
         .asUint8List();
   }
 
-  void _getCustomMarker() async {
+
+
+  /*void _getCustomMarker() async {
     final Uint8List romeoMarkerIcon =
     await getBytesFromAsset('assets/images/romeo.jpg', 150);
     romeoIcon = BitmapDescriptor.fromBytes(romeoMarkerIcon);
- 
+
     final Uint8List julietMarkerIcon =
     await getBytesFromAsset('assets/images/juliet.jpg', 150);
     julietIcon = BitmapDescriptor.fromBytes(julietMarkerIcon);
@@ -77,32 +62,23 @@ class _MapViewState extends State<MapView> {
     arenaIcon = BitmapDescriptor.fromBytes(arenaMarkerIcon);
 
     setState(() {});
-  }
+  }*/
 
 
 
   @override
   Widget build(BuildContext context) {
-    final Marker veronaMarker = createMarker("verona", _verona, veronaIcon, "Verona");
-    final Circle veronaCircle = Circle(circleId: CircleId("veronaCircle"),
-        center: _verona,radius: 100,
-        strokeColor: Colors.blue,strokeWidth: 5 );
-    final Marker romeoMarker =
-    createMarker('romeo', _romeoHome, romeoIcon, 'Casa di Romeo');
-    final Marker julietMarker =
-    createMarker('juliet', _julietHome, julietIcon, 'Casa di Giulietta');
-    final Marker arenaMarker =
-    createMarker('arena', _veronaArena, arenaIcon, 'Arena di Verona');
+
 
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){_goToTheLake();},),
+
       appBar: AppBar(title: const Text("Map Sample")),
       body: GoogleMap(
         myLocationButtonEnabled: true,
         indoorViewEnabled: true,
-        circles: {veronaCircle},
+
         mapType: MapType.normal,
-        markers: {romeoMarker, julietMarker, arenaMarker, veronaMarker},
+        markers: markers.toSet(),
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
