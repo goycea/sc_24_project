ccb952b4676d37aadc0027a3414b91ad9a12fc03
Alperen Göyce <goycea@gmail.com>
2024-02-23 20:06:16 +0300

    icons added
    icons added to map view to filter mark

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -4,6 +4,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import '../utils/list_constants.dart';
 
@@ -19,29 +20,31 @@ class _MapViewState extends State<MapView> {
       Completer<GoogleMapController>();
 
   BitmapDescriptor assemblyArea = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor building = BitmapDescriptor.defaultMarker;
+  bool assemblyAreaVisible = true;
+  bool buildingVisible = true;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(39.924635, 32.862448),
     zoom: 16.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-      target: LatLng(39.9019095674532, 32.8601492660582),
-      zoom: 15.151926040649414);
+  var box = Hive.box('user');
 
+  Set<Marker> assemblyMarkers = {};
+  Set<Marker> buildingMarkers = {};
   Set<Marker> markers = {};
 
   @override
   void initState() {
     super.initState();
-    _getCustomMarker();
-    markers = coordinateList.map((e) {
-      return Marker(
-          icon: assemblyArea,
-          markerId: MarkerId("${e.name}"),
-          infoWindow: InfoWindow(title: "${e.name}"),
-          position: LatLng(e.coordinates.first, e.coordinates.last));
-    }).toSet();
+    box.get("assemblyAreaVisible") == null
+        ? assemblyAreaVisible = true
+        : assemblyAreaVisible = box.get("assemblyAreaVisible");
+    box.get("buildingVisible") == null
+        ? buildingVisible = true
+        : buildingVisible = box.get("buildingVisible");
+    _getAllMarkers();
   }
 
   Future<Uint8List> getBytesFromAsset(String path, int width) async {
@@ -54,10 +57,32 @@ class _MapViewState extends State<MapView> {
         .asUint8List();
   }
 
-  void _getCustomMarker() async {
+  Future<void> _getAssembly() async {
     final Uint8List assemblyIcon =
-        await getBytesFromAsset('assets/images/assembly.jpg', 150);
+        await getBytesFromAsset('assets/images/assembly.png', 150);
     assemblyArea = BitmapDescriptor.fromBytes(assemblyIcon);
+    assemblyMarkers = coordinateList.map((e) {
+      return Marker(
+          icon: assemblyArea,
+          markerId: MarkerId("${e.name}"),
+          infoWindow: InfoWindow(title: "${e.name}"),
+          position: LatLng(e.coordinates.first, e.coordinates.last));
+    }).toSet();
+  }
+
+  Future<void> _getBuildingMarker() async {
+    final Uint8List assemblyIcon =
+        await getBytesFromAsset('assets/images/building.png', 150);
+    building = BitmapDescriptor.fromBytes(assemblyIcon);
+    buildingMarkers = {};
+  }
+
+  Future<void> _getAllMarkers() async {
+    markers = {};
+    assemblyMarkers.isEmpty ? await _getAssembly() : null;
+    buildingMarkers.isEmpty ? await _getBuildingMarker() : null;
+    assemblyAreaVisible ? markers.addAll(assemblyMarkers) : null;
+    buildingVisible ? markers.addAll(buildingMarkers) : null;
     setState(() {});
   }
 
@@ -65,13 +90,39 @@ class _MapViewState extends State<MapView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Map Sample"), actions: [
+        IconButton(
+            onPressed: () async {
+              if (buildingVisible) {
+                buildingVisible = false;
+                await _getAllMarkers();
+              } else {
+                buildingVisible = true;
+                await _getAllMarkers();
+              }
+              box.put("buildingVisible", buildingVisible);
+            },
+            icon: Icon(buildingVisible ? Icons.home : Icons.add_home)),
+        IconButton(
+            onPressed: () async {
+              if (assemblyAreaVisible) {
+                assemblyAreaVisible = false;
+                await _getAllMarkers();
+              } else {
+                assemblyAreaVisible = true;
+                await _getAllMarkers();
+              }
+              box.put("assemblyAreaVisible", assemblyAreaVisible);
+            },
+            icon: Icon(assemblyAreaVisible
+                ? Icons.people_alt
+                : Icons.person_add_alt_rounded)),
         IconButton(onPressed: () {}, icon: Icon(Icons.filter_alt))
       ]),
       body: GoogleMap(
         myLocationButtonEnabled: true,
         indoorViewEnabled: true,
         mapType: MapType.normal,
-        markers: markers.toSet(),
+        markers: markers,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
