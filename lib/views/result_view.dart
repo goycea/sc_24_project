d17ae24d97a9451de7b64990ccd58fdb95e79e37
Alperen Göyce <goycea@gmail.com>
2024-02-23 10:44:35 +0300

    result view added

--- lib/views/result_view.dart
+++ lib/views/result_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sc_24_project/managers/auth_manager.dart';
+import 'package:sc_24_project/models/result_model.dart';
 
 class ResultView extends StatefulWidget {
   const ResultView({super.key});
@@ -8,8 +11,49 @@ class ResultView extends StatefulWidget {
 }
 
 class _ResultViewState extends State<ResultView> {
+  AuthenticationManager readAuthManager() =>
+      context.read<AuthenticationManager>();
+  AuthenticationManager watchAuthManager() =>
+      context.watch<AuthenticationManager>();
+
+  ResultModel resultModel = ResultModel();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    resultModel = readAuthManager().resultModel;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Result"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Card(
+                child: Text(
+              resultModel.totalValue.toString(),
+              style: const TextStyle(fontSize: 20),
+            )),
+            Card(
+                child: Text(resultModel.groundType.toString(),
+                    style: const TextStyle(fontSize: 20))),
+            Card(
+                child: Text(resultModel.pGA.toString(),
+                    style: const TextStyle(fontSize: 20))),
+            Card(
+              child: Text(resultModel.index.toString(),
+                  style: const TextStyle(fontSize: 20)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
